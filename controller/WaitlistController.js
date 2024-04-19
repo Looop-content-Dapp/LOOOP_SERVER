fb73f71d0c355d89913b7ee2e8e02e6363cefe28
collinstb01@gmail.com <collinstb01@gmail.com>
2024-04-19 23:36:28 +0100

    feat: remove .env

--- controller/WaitlistController.js
+++ controller/WaitlistController.js
@@ -2,6 +2,9 @@ const { Web5 } = require("@web5/api");
 
 const registerUser = async (req, res) => {
   try {
+    const { web5, did: aliceDid } = await Web5.connect();
+    console.log(aliceDid);
+
     return res
       .status(200)
       .json({ status: 200, msg: "Registered user successfully" });
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const waitListRouter = require("./routes/WaitlistRoute");
-const { Web5 } = require("@web5/api");
 
 app.use("/waitlist", waitListRouter);
 
@@ -11,7 +10,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, async () => {
-  const { web5, did: aliceDid } = await Web5.connect();
-
   console.log(`App listening on PORT ${PORT}`);
 });
